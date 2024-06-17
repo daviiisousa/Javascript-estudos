5cb30c82ae020f311fa582b83912ae8d758ae83c
daviiisousa <sousadavi248@gmail.com>
2024-06-17 16:24:58 -0300

    linkando arquivos

---
+++ meuTeste006/index.js
@@ -0,0 +1,4 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+const loadingElement = document.getElementById('loading')
+const postsContainer = document.getElementById('posts-container')
