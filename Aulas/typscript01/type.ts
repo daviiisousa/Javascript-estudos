d412659e50de774174eb2c37d2c0e59cfcc71c5f
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-07-03 19:20:00 -0300

    onnfigurando ts

---
+++ Aulas/typscript01/type.ts
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+const num1 = 2;
+const num2 = 3;
+console.log(num1 + num2);
---
+++ dist/type.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Hello World');
+const num1 = 2;
+const num2 = 3;
+console.log(num1 + num2);
