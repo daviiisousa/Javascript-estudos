fd1892791abae31c9994508ac55eac322e53bd08
daviiisousa <sousadavi248@gmail.com>
2024-10-20 18:56:52 -0300

    dom

--- origamid/AnimaisFantasticos/javascript/app.js
+++ origamid/AnimaisFantasticos/javascript/app.js
@@ -1,23 +1,56 @@
-const tabMenu = document.querySelectorAll('.js-tabMenu li')
-const tabContet = document.querySelectorAll('.js-tabContent section')
-tabContet[0].classList.add('ativo')
+function initTab() {
+  const tabMenu = document.querySelectorAll(".js-tabMenu li");
+  const tabContet = document.querySelectorAll(".js-tabContent section");
+  tabContet[0].classList.add("ativo");
 
-function activeTab(index){
+  function activeTab(index) {
     tabContet.forEach((section) => {
-        section.classList.remove('ativo')
-    })
-    tabContet[index].classList.add('ativo')
+      section.classList.remove("ativo");
+    });
+    tabContet[index].classList.add("ativo");
+  }
 
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
 }
 
-tabMenu.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        activeTab(index) 
-    })
-})
+initTab();
 
-//percorrendo um array like e utilizando innerHtml
-// tabContet.forEach((item) => {
-//     console.log(item.innerHTML);
-    
-// })
+function innitAcordion() {
+  const accordianList = document.querySelectorAll(".js-accordion dt");
+
+  accordianList[0].nextElementSibling.classList.add("ativo");
+
+  function acctiveAccordion() {
+    this.classList.toggle("ativo");
+    this.nextElementSibling.classList.toggle("ativo");
+  }
+
+  accordianList.forEach((item) => {
+    item.addEventListener("click", acctiveAccordion);
+  });
+}
+
+innitAcordion();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-section");
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - 500;
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+
+initAnimacaoScroll();
